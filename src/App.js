117495ef3501dda3ee139c643042c18972e5b084
Zareef <zareef.m@hotmail.com>
2024-11-24 16:32:17 +1100

    Add handler for moves in game component

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  
+function Board({ xIsNext, squares, onPlay }) {  
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -23,8 +20,7 @@ function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -63,7 +59,8 @@ export default function Game() {
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
-
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
   
   return (
