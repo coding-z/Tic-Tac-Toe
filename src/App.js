c11f31f7ac1472a69db5399f67b78b00ab74503c
Zareef <zareef.m@hotmail.com>
2024-11-24 17:58:10 +1100

    Fix display bug

--- src/App.js
+++ src/App.js
@@ -62,8 +62,7 @@ export default function Game() {
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
-    setCurrentMove(history.length - 1);
-    setHistory([...history, nextSquares]);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove) {
