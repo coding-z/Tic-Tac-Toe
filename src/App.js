fcfb2e30aafc85b4e5e519d8aafd421510626d7e
coding-z <zareef.m@hotmail.com>
2024-11-25 14:37:08 +1100

    Updated CSS styles

--- src/App.js
+++ src/App.js
@@ -32,24 +32,20 @@ function Board({ xIsNext, squares, onPlay }) {
   }
   
   return (
-    <>
+    <div className="game-main">
       <div className="status">{status}</div>
-      <div className="board-row">
+      <div className="game-board">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
-    </>
+    </div>
   );
 }
 
@@ -110,9 +106,7 @@ export default function Game() {
   
   return (
     <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       <History history={history} onMoveChange={setCurrentMove} />
     </div>
   );
